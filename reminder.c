08398cf1ec6820e6a3528a689e0d595dd7e83e7f
Emy Gaete <167256512+Emyowo204@users.noreply.github.com>
2024-09-06 16:06:05 -0400

    Actualización del comando set recordatorio

--- reminder.c
+++ reminder.c
@@ -4,27 +4,23 @@ void never_lyrics() {
 	printf("Never gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\n");
 	printf("Never gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\n");
 }
-
 int set_recordatorio(char *input, char **cmd) {
-    char *token = strtok(input, " ");
-    int value = 0;
-    while (token != NULL) {
-        value++;
-        token = strtok(NULL, " ");
-    }
+    int value = getTokenNum(input, " ");
+    int status;
     if(value>=3) {
     	if(strcmp(cmd[1],"recordatorio")==0) {
     		int seconds = atoi(cmd[2]);
             pid_t pid = fork();
     		if (pid == 0) {
                 sleep(seconds);
-                //printf("\nALARMA: Recordatorio de hace %d segundos\n", seconds);
-                //for(int i=3; i<value; i++)
-                //    printf("%s ",cmd[i]);
-                //printf("\n");
+                printf("\nALARMA: Recordatorio de hace %d segundos\n", seconds);
+                for(int i=3; i<value; i++)
+                    printf("%s ",cmd[i]);
+                printf("\n");
                 exit(0);
             } else {
                 printf("Recordatorio establecido para %d segundos\n", seconds);
+                wait(&status);
             }
             return(0);
     	}
